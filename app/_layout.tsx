614ead798e3abcf3f1ffb224d302464ecc8b3e62
Saleem Maini <saleem.maini@miniandmore.co>
2026-01-12 23:33:35 +0200

    add react query package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
+import { queryClient } from '@/lib/react-query'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import { useColorScheme } from 'react-native'
 
@@ -6,11 +8,13 @@ export default function RootLayout() {
   const colorScheme = useColorScheme()
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-        <Stack.Screen name='modal' options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+          <Stack.Screen name='modal' options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
