a72d9bc5ad1cdae7eeedcee3c8c55ce2f4a96d5e
Saleem Maini <saleem.maini@miniandmore.co>
2026-01-13 01:07:37 +0200

    remove color scheme handling and simplify theme usage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,12 @@
 import { queryClient } from '@/lib/react-query'
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
-import { useColorScheme } from 'react-native'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
-
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={DefaultTheme}>
         <Stack>
           <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
         </Stack>
