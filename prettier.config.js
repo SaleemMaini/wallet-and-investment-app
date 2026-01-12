743192964e947ddddbcb8900a34d1b59e61fb063
Saleem Maini <saleem.maini@miniandmore.co>
2026-01-12 21:41:30 +0200

    add prettier configs

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  semi: false,
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  trailingComma: 'none',
+  useTabs: false,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tailwindAttributes: ['className']
+}
